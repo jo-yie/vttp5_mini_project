74c07c6e179bb5a65baddb691c59b7697533f5f1
Jo Yie <joyieleong@gmail.com>
2024-12-28 17:16:54 +0800

    added restController folder

--- src/main/java/vttp5_mini_project/restController/AppRestController.java
+++ src/main/java/vttp5_mini_project/restController/AppRestController.java
@@ -1,7 +1,6 @@
-package vttp5_mini_project.controller;
+package vttp5_mini_project.restController;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
